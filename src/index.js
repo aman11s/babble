ca0d14c694d24b5108441c1e41dfa01cec5b0322
Aman Singh <reachout.amansingh@gmail.com>
2022-08-07 23:15:01 +0530

    chore : mockbee cra

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "./App";
+import { makeServer } from "./server";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+// Call make Server
+makeServer();
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
