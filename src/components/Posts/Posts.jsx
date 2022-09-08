33b6f3dd4295a8bfd93637c7d6d98a2f2064d7e8
Aman Singh <reachout.amansingh@gmail.com>
2022-09-09 01:46:07 +0530

    fix : remove loader from posts

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,25 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts } from "../../features";
 import { PostCard } from "../PostCard/PostCard";
-import { ClipLoader } from "react-spinners";
-import { Box } from "@mui/material";
 
 export const Posts = () => {
   const dispatch = useDispatch();
 
-  const { posts, status } = useSelector((store) => store.posts);
+  const { posts } = useSelector((store) => store.posts);
 
   useEffect(() => {
     dispatch(getAllPosts());
   }, [dispatch]);
-
-  if (status === "pending") {
-    return (
-      <Box sx={{ textAlign: "center" }}>
-        <ClipLoader speedMultiplier={3} size={30} />
-      </Box>
-    );
-  }
 
   return (
     <>
