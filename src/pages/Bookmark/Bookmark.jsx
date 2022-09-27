22e2fcc657114f71b75e5027e2eafe34290fc599
Aman Singh <reachout.amansingh@gmail.com>
2022-09-28 02:37:43 +0530

    feat : show all bookmark posts

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,9 +1,29 @@
+import { Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import { PostCard } from "../../components";
 
 export const Bookmark = () => {
+  const { bookmarks } = useSelector((store) => store.bookmarks);
+
   return (
     <>
-      <h1>Bookmark</h1>
+      <Typography
+        sx={{ textAlign: "center", fontWeight: "600", my: 1 }}
+        variant="h5"
+      >
+        My Bookmarks
+      </Typography>
+
+      {bookmarks.length ? (
+        bookmarks.map((post) => {
+          return <PostCard key={post.id} post={post} />;
+        })
+      ) : (
+        <Typography sx={{ textAlign: "center", my: 4 }} variant="h6">
+          You haven't bookmarked any post
+        </Typography>
+      )}
     </>
   );
 };
