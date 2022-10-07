8f7e2d808b6171dc3cbbb167758d3367f663026a
Aman Singh <reachout.amansingh@gmail.com>
2022-10-08 00:42:23 +0530

    feat : update profile page

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,90 @@
+import { Avatar, Box, Button, Link, Typography } from "@mui/material";
+import React from "react";
+import { CommonBox } from "../CommonBox/CommonBox";
+import InsertLinkRoundedIcon from "@mui/icons-material/InsertLinkRounded";
+import { useSelector } from "react-redux";
+
+export const ProfileCard = ({ singleUser }) => {
+  const { posts } = useSelector((store) => store.posts);
+
+  const loggedUserPosts = posts.filter(
+    (post) => post.username === singleUser?.username
+  );
+
+  if (singleUser) {
+    const {
+      firstName,
+      lastName,
+      avatarURL,
+      username,
+      followers,
+      following,
+      bio,
+      websiteLink,
+    } = singleUser;
+
+    return (
+      <>
+        <CommonBox>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-around",
+              flexWrap: "wrap",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <Box>
+              <Avatar
+                sx={{
+                  height: 120,
+                  width: 120,
+                }}
+                alt="user-avatar"
+                src={avatarURL}
+              />
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+              <Typography sx={{ fontWeight: 600 }} variant="h6">
+                <Box component="span">{firstName}</Box>{" "}
+                <Box component="span">{lastName}</Box>
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }} variant="body1">
+                @{username}
+              </Typography>
+              <Typography variant="body1">{bio}</Typography>
+              <Link
+                sx={{ display: "flex", alignItems: "center" }}
+                href={websiteLink}
+                underline="hover"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <InsertLinkRoundedIcon />
+                <Box sx={{ ml: 1 }} component="span">
+                  {websiteLink}
+                </Box>
+              </Link>
+              <Box sx={{ display: "flex", gap: 2 }}>
+                <Box sx={{ textAlign: "center" }}>
+                  <Box>{loggedUserPosts?.length}</Box>
+                  <Typography variant="body1">Posts</Typography>
+                </Box>
+                <Box sx={{ textAlign: "center" }}>
+                  <Box>{followers.length}</Box>
+                  <Typography variant="body1">Followers</Typography>
+                </Box>
+                <Box sx={{ textAlign: "center" }}>
+                  <Box>{following.length}</Box>
+                  <Typography variant="body1">Following</Typography>
+                </Box>
+              </Box>
+              <Button variant="outlined">Edit Profile</Button>
+            </Box>
+          </Box>
+        </CommonBox>
+      </>
+    );
+  }
+};
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,20 +1,66 @@
-import { Button } from "@mui/material";
-import React from "react";
-import { useDispatch } from "react-redux";
-import { logoutHandler } from "../../features";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { PostCard, ProfileCard } from "../../components";
+import { getAllPosts, getUser, getUserPost } from "../../features";
+import { useEffect } from "react";
+import { ClipLoader } from "react-spinners";
 
 export const Profile = () => {
   const dispatch = useDispatch();
 
+  const { user, status: userStatus } = useSelector((store) => store.user);
+  const { status: postStatus } = useSelector((store) => store.posts);
+
+  const [singleUser, setSingleUser] = useState();
+  const [singleUserPost, setSingleUserPost] = useState();
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { meta: userMeta, payload: userPayload } = await dispatch(
+          getUser({ username: user.username })
+        );
+        if (userMeta.requestStatus === "fulfilled") {
+          setSingleUser(userPayload.getUser);
+        }
+        const { meta: postMeta, payload: postPayload } = await dispatch(
+          getUserPost({ username: user.username })
+        );
+        if (postMeta.requestStatus === "fulfilled") {
+          setSingleUserPost(postPayload.getUserPost);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [dispatch, user]);
+
+  if (userStatus === "pending") {
+    return (
+      <Box sx={{ textAlign: "center", my: 4 }}>
+        <ClipLoader speedMultiplier={3} size={30} />
+      </Box>
+    );
+  }
+
   return (
     <>
-      <Button
-        onClick={() => dispatch(logoutHandler())}
-        variant="outlined"
-        color="error"
-      >
-        logout
-      </Button>
+      <ProfileCard singleUser={singleUser} />
+
+      {postStatus === "pending" && (
+        <Box sx={{ textAlign: "center", my: 4 }}>
+          <ClipLoader speedMultiplier={3} size={30} />
+        </Box>
+      )}
+
+      {singleUserPost?.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </>
   );
 };
