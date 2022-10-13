1ccab37410bb75f7d465d2e5e166e3b984a11c38
Aman Singh <reachout.amansingh@gmail.com>
2022-10-13 23:13:53 +0530

    fix : change avatar to user icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ import logo from "../../logo.png";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import ExploreRoundedIcon from "@mui/icons-material/ExploreRounded";
 import BookmarkRoundedIcon from "@mui/icons-material/BookmarkRounded";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 import { NavLink, useLocation } from "react-router-dom";
 import { grey } from "@mui/material/colors";
-import { useSelector } from "react-redux";
 
 const icons = [
   {
@@ -24,12 +24,16 @@ const icons = [
     comp: <BookmarkRoundedIcon fontSize="large" />,
     route: "/bookmark",
   },
+  {
+    id: 4,
+    comp: <PersonRoundedIcon fontSize="large" />,
+    route: "/profile",
+  },
 ];
 
 export const Navbar = () => {
   const theme = useTheme();
   const { pathname } = useLocation();
-  const { user } = useSelector((store) => store.user);
 
   const getActiveStyle = ({ isActive }) => ({
     textDecoration: "none",
@@ -66,29 +70,16 @@ export const Navbar = () => {
                 alt="logo"
               />
             </NavLink>
-            <List sx={{ display: "flex", py: 1 }}>
+            <List sx={{ display: "flex" }}>
               {icons.map(({ id, comp, route }) => {
                 return (
-                  <ListItem key={id} sx={{ mx: 2 }} disablePadding>
+                  <ListItem key={id} sx={{ mx: 1.5 }} disablePadding>
                     <NavLink style={getActiveStyle} to={route}>
                       {comp}
                     </NavLink>
                   </ListItem>
                 );
               })}
-              <ListItem sx={{ mx: 1 }} disablePadding>
-                <NavLink to="/profile">
-                  <Avatar
-                    alt="user-icon"
-                    src={user.avatarURL}
-                    sx={{
-                      border: `1px solid ${grey[500]}`,
-                      width: 45,
-                      height: 45,
-                    }}
-                  />
-                </NavLink>
-              </ListItem>
             </List>
           </Box>
         </Box>
