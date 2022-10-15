858fe80d822baa4f756d3e926ea1d6f29fb775bc
Aman Singh <reachout.amansingh@gmail.com>
2022-10-15 16:33:18 +0530

    feat : search bar comp

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { IconButton, InputBase, Paper, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const Explore = () => {
   return (
     <>
-      <h1>Explore</h1>
+      <Typography
+        sx={{ textAlign: "center", fontWeight: "600", my: 1 }}
+        variant="h5"
+      >
+        Explore
+      </Typography>
+
+      <Paper
+        component="form"
+        sx={{
+          p: "4px 6px",
+          display: "flex",
+          my: 4,
+        }}
+      >
+        <InputBase sx={{ ml: 1.5, flex: 1 }} placeholder="Search People" />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </>
   );
 };
