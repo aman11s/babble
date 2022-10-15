a8faac3f40279de10764800c85063f33d1fe7905
Aman Singh <reachout.amansingh@gmail.com>
2022-10-15 22:18:25 +0530

    feat : post loader

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,13 +1,19 @@
+import { Box } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts } from "../../features";
 import { getSortedPosts } from "../../utils";
 import { PostCard } from "../PostCard/PostCard";
+import { ClipLoader } from "react-spinners";
 
 export const Posts = () => {
   const dispatch = useDispatch();
 
-  const { posts, sortBy } = useSelector((store) => store.posts);
+  const {
+    posts,
+    sortBy,
+    status: postStatus,
+  } = useSelector((store) => store.posts);
   const { user } = useSelector((store) => store.user);
 
   const followingUsers = user.following.map(({ username }) => username);
@@ -22,6 +28,14 @@ export const Posts = () => {
   useEffect(() => {
     dispatch(getAllPosts());
   }, [dispatch]);
+
+  if (postStatus === "pending") {
+    return (
+      <Box sx={{ textAlign: "center", my: 4 }}>
+        <ClipLoader speedMultiplier={3} size={30} />{" "}
+      </Box>
+    );
+  }
 
   return (
     <>
