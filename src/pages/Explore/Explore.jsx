99b33d1cedcfb5d71a2fd9e547c933e35c4be29b
Aman Singh <reachout.amansingh@gmail.com>
2022-10-18 23:08:15 +0530

    feat : search funtionality

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from "react";
-import { Box, IconButton, InputBase, Paper, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  MenuItem,
+  Paper,
+  Typography,
+  MenuList,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllPosts } from "../../features";
-import { getSortedPosts } from "../../utils/utils";
-import { PostCard } from "../../components";
+import { getAllPosts, getAllUsers } from "../../features";
+import { getSortedPosts, getUsersByQuery } from "../../utils/utils";
+import { PostCard, UserHorizontalCard } from "../../components";
 import { ClipLoader } from "react-spinners";
 
 export const Explore = () => {
@@ -12,8 +20,24 @@ export const Explore = () => {
 
   const { posts, status: postStatus } = useSelector((store) => store.posts);
 
+  const [query, setQuery] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+
   useEffect(() => {
     dispatch(getAllPosts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { meta, payload } = await dispatch(getAllUsers());
+        if (meta.requestStatus === "fulfilled") {
+          setAllUsers(payload.getAllUsers);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    })();
   }, [dispatch]);
 
   const sortedPosts = getSortedPosts(posts, "Trending");
@@ -25,6 +49,8 @@ export const Explore = () => {
       </Box>
     );
   }
+
+  const filteredUsers = getUsersByQuery(allUsers, query);
 
   return (
     <>
@@ -40,14 +66,33 @@ export const Explore = () => {
         sx={{
           p: "4px 6px",
           display: "flex",
-          my: 4,
+          mt: 4,
+          mb: 1,
         }}
       >
-        <InputBase sx={{ ml: 1.5, flex: 1 }} placeholder="Search People" />
+        <InputBase
+          onChange={(e) => setQuery(e.target.value)}
+          sx={{ ml: 1.5, flex: 1 }}
+          placeholder="Search People"
+        />
         <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
           <SearchIcon />
         </IconButton>
       </Paper>
+
+      {filteredUsers && (
+        <Paper sx={{ maxHeight: "12rem", overflowY: "auto" }} elevation={2}>
+          {filteredUsers.map((user) => {
+            return (
+              <MenuList key={user.id}>
+                <MenuItem divider>
+                  <UserHorizontalCard user={user} searchCard={true} />
+                </MenuItem>
+              </MenuList>
+            );
+          })}
+        </Paper>
+      )}
 
       {sortedPosts.map((post) => (
         <PostCard key={post.id} post={post} />
